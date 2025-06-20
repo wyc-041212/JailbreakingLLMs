e666e45139894b19d43cfc249c25413167f8dd72
wyc-041212 <1160885822@qq.com>
2025-06-20 20:46:09 +0800

    models

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ if __name__ == '__main__':
         "--target-model",
         default = "vicuna-13b-v1.5", #TODO changed
         help = "Name of target model.",
-        choices=["vicuna-13b-v1.5", "llama-2-7b-chat-hf", "gpt-3.5-turbo-1106", "gpt-4-0125-preview", "claude-instant-1.2", "claude-2.1", "gemini-pro",]
+        choices=["vicuna-13b-v1.5", "llama-2-7b-chat-hf", "gpt-3.5-turbo-1106", "gpt-4-0125-preview", "claude-instant-1.2", "claude-2.1", "gemini-pro", "qwen-2.5-7b-instruct", "deepseek-r1-distill-qwen-7b"]
     )
     parser.add_argument(
         "--target-max-n-tokens",
